2296ce470e4ec58960b2caa02f52c4ae03af38ab
xzyxrene <rivamraj@gmail.com>
2023-02-21 04:50:25 +0530

    first instance of working code!

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,62 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
 
-  void answerQuestion() {
-    questionIndex++;
-    print(questionIndex);
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  final questions = const [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Red', 'Blue', 'Green', 'Yellow'],
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+    },
+    {
+      'questionText': 'Which age group do you belong in?',
+      'answers': ['Below 21', '21 to 45', '46 to 60', 'Above 60'],
+    },
+  ];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favourite color?',
-      'What\'s your favourite animal?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Text(
-              questions[questionIndex],
-            ),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 2'),
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer 3'),
-              onPressed: answerQuestion,
-            ),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('Yay... You did it!'),
+              ),
       ),
     );
   }
