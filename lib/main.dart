2948ea6205d8c2bbd20f36ce10f85bb665bdaa19
xzyxrene <rivamraj@gmail.com>
2023-02-20 15:15:19 +0530

    till stateless widget only

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer Chosen!');
+    questionIndex++;
+    print(questionIndex);
   }
 
   @override
@@ -21,7 +24,9 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question!'),
+            Text(
+              questions[questionIndex],
+            ),
             ElevatedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
